901963aa99b6e7a6d9a72da9a46000ab7810dd32
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-06-24 04:57:59 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { AssetsModule } from './assets/assets.module';
+import { OrdersModule } from './orders/orders.module';
+
+@Module({
+  imports: [PrismaModule, AssetsModule, OrdersModule],
+})
+export class AppModule {}
